5dc89b16977080d6c64d300a47191b364cd64bb0
Hanna12674 <hannyteshome@gmail.com>
2024-12-10 10:39:37 -0500

    04/12/2024

---
+++ src/main/java/org/hannaweldehana/weldehana_hanna_insuranceclaimnotificationsystem/WeldehanaHannaInsuranceClaimNotificationSystemApplication.java
@@ -0,0 +1,13 @@
+package org.hannaweldehana.weldehana_hanna_insuranceclaimnotificationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+public class WeldehanaHannaInsuranceClaimNotificationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeldehanaHannaInsuranceClaimNotificationSystemApplication.class, args);
+    }
+}
